ce44972ffe61e52b96c02b8aa7ac51cdec6fede6
Catharina <catharina.sorum@outlook.com>
2019-11-14 11:25:56 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Component from './components/Component'
+import List from './components/List'
+
+const App = () => {
+
+  const names = ['Lars', 'Bettina', 'Per']
+
+  return (
+    <div className="App">
+      <h1>Min første react app kanskje inshallah</h1>
+      <Component />
+      {
+        names.map( name => <List name={name} /> )
+      }
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = (props) => {
+    return(
+        <div>
+            <p>Dette er den første subkomponenten. Den er veldig enkel, og den returnerer bare denne teksten.</p>
+        </div>
+    )
+}
+
+export default Component
